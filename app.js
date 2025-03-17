7d3b24b14e66591166e87678b119acd680c0d275
omarcc369 <gerardoomar247act@ciencias.unam.mx>
2025-03-17 13:58:10 -0600

    Ya el reto todo listo

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+let recuadro = document.getElementById('amigo');
+let lista = document.getElementById('listaAmigos');
+
+function agregarAmigo() {
+
+    let amigo = recuadro.value;
+
+    if (amigo == "") {
+        alert("Debe ingresar un nombre válido por favor");
+        estableceBlanco();
+        return;
+    }
+
+    amigos.push(amigo);
+    estableceBlanco();
+    console.log(amigos);
+    mostrarAmigos();
+
+}
+
+function estableceBlanco() {
+
+    recuadro.value = "";
+}
+
+function mostrarAmigos() {
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
+
+}
+
+
+function sortearAmigo() {
+
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    lista.innerHTML = "<li>" + `El amigo sorteado es ${amigoSorteado}` + "</li>";
+    lista.value = "";
+    amigos = [];
+}
